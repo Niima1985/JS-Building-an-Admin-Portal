3760ae29ba9479a0a69a4cafce09269046c40009
Niima1985 <67212610+Niima1985@users.noreply.github.com>
2022-06-04 12:40:53 -0700

    Changes all files with solutions

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,52 @@
 
 // Your Code Here
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')// This is the address of a server-side function that are invoking with fetch request using GET method (by dafault)
+
+    let bookLists = await response.json() // parse the response using response.json() and save it to variable
+
+    bookLists.forEach(renderBook) //Once the array of books is retrieved, using .forEach to add cards for each book to the DOM.
+}
+
+
+function renderBook(book) {
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+    
+    let deleteButton = document.createElement('button')
+    deleteButton.textContent = 'Delete'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    deleteButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/removeBook/{bookId}',{
+            method: 'DELETE'
+        })
+    })
+    li.append(quantityInput, saveButton)
+    li.append(quantityInput, deleteButton)
+
+    root.append(li)
+}
+
+main()
--- public/index.js
+++ public/index.js
@@ -1,24 +1,29 @@
 async function main() {
 
-    let response = await fetch('http://localhost:3001/listBooks')
-    let books = await response.json()
+    let response = await fetch('http://localhost:3001/listBooks')// This is the address of a server-side function we're invoking with our fetch request and by default fetch sending resquest using GET method
 
-    books.forEach(renderBook)
+    let books = await response.json()  // use await response.json() parse the response from JSON and save the result to a variable and once parse the response will be an array of book objects
+    console.log(books)
+    books.forEach(renderBook)// use for adding cards for each book to the DOM
 }
 
-function renderBook(book) {
+function renderBook(book){
     let bookContainer = document.querySelector('.book-container')
     bookContainer.innerHTML += `
-        <div class="col-sm-3">
-            <div class="card" style="width: 100%;">
-                ${book.imageURL ? `
-                    <img class="card-img-top" src="${book.imageURL}" />
-                `
-                : ``}
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
-                    <p class="card-text">${book.description}</p>
+        <div class="ui card">
+            ${book.imageURL ?`
+                <div class="image">
+                    <img src="${book.imageURL}" />
+                </div>
+            `
+            :``}
+            <div class="content">
+                <a class="header">${book.title}</a>
+                <div class="meta">
+                    <span class="date">Available: ${book.quantity}</span>
+                </div>
+                <div class="description">
+                    ${book.description}
                 </div>
             </div>
         </div>
